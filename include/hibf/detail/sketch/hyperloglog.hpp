c3557608c5155d4b0e139ef2a2539b4edf3e456f
Enrico Seiler <enrico.seiler@hotmail.de>
2023-06-01 15:26:41 +0200

    [MISC] Split detail/sketch

--- include/hibf/detail/sketch/hyperloglog.hpp
+++ include/hibf/detail/sketch/hyperloglog.hpp
@@ -19,9 +19,6 @@
 #include <hibf/contrib/xxhash/xxh3.h>
 #include <hibf/platform.hpp>
 
-#include <x86/avx.h>
-#include <x86/avx2.h>
-
 namespace hibf::sketch
 {
 
@@ -94,34 +91,7 @@ public:
      *
      * @return Estimated cardinality value.
      */
-    double estimate() const
-    {
-        // compute indicator formula
-        double sum = 0.0;
-        for (uint8_t c : M_)
-        {
-            sum += exp2_rcp[c];
-        }
-        double estimate = alphaMM_ / sum;
-
-        // use linear counting of zeros for small values
-        if (estimate <= 2.5 * m_)
-        {
-            uint32_t zeros = 0;
-
-            for (size_t i = 0; i < m_; ++i)
-            {
-                if (!M_[i])
-                    ++zeros;
-            }
-
-            if (zeros != 0u)
-            {
-                estimate = m_ * std::log(static_cast<double>(m_) / static_cast<double>(zeros));
-            }
-        }
-        return estimate;
-    }
+    double estimate() const;
 
     /**
      * Merges the estimate from 'other' into this object
@@ -152,105 +122,7 @@ public:
      *
      * @return estimated cardinality of the new merged sketch
      */
-    double merge_and_estimate_SIMD(hyperloglog const & other)
-    {
-        assert(m_ == other.m_);
-        assert(b_ >= 5);
-
-        // this is safe when b_ is at least 5. Then, M_'s size in bits is
-        // 2^x * 2^5 * 8 = 2^x * 256 >= 256, where x is an integer >= 1
-        // also, M_ is 256 bit aligned in memory
-        simde__m256i * it = reinterpret_cast<simde__m256i *>(&*(M_.begin()));
-        simde__m256i const * other_it = reinterpret_cast<simde__m256i const *>(&*(other.M_.begin()));
-        simde__m256i * end = reinterpret_cast<simde__m256i *>(&*(M_.end()));
-
-        simde__m256 packed_sum = simde_mm256_set1_ps(0.0f);
-
-        for (; it != end; ++it, ++other_it)
-        {
-            // this merges the registers by computing the byte-wise maximum
-            *it = simde_mm256_max_epu8(*it, *other_it);
-
-            // get pointer to iterate over the single merged registers
-            uint8_t * reg_it = reinterpret_cast<uint8_t *>(it);
-
-            // get floats with two to the power of minus the value in the merged registers and sum up
-            packed_sum = simde_mm256_add_ps(packed_sum,
-                                            simde_mm256_set_ps(exp2_rcp[*reg_it],
-                                                               exp2_rcp[*(reg_it + 1)],
-                                                               exp2_rcp[*(reg_it + 2)],
-                                                               exp2_rcp[*(reg_it + 3)],
-                                                               exp2_rcp[*(reg_it + 4)],
-                                                               exp2_rcp[*(reg_it + 5)],
-                                                               exp2_rcp[*(reg_it + 6)],
-                                                               exp2_rcp[*(reg_it + 7)]));
-
-            // repeat 3 times...
-            packed_sum = simde_mm256_add_ps(packed_sum,
-                                            simde_mm256_set_ps(exp2_rcp[*(reg_it + 8)],
-                                                               exp2_rcp[*(reg_it + 9)],
-                                                               exp2_rcp[*(reg_it + 10)],
-                                                               exp2_rcp[*(reg_it + 11)],
-                                                               exp2_rcp[*(reg_it + 12)],
-                                                               exp2_rcp[*(reg_it + 13)],
-                                                               exp2_rcp[*(reg_it + 14)],
-                                                               exp2_rcp[*(reg_it + 15)]));
-
-            packed_sum = simde_mm256_add_ps(packed_sum,
-                                            simde_mm256_set_ps(exp2_rcp[*(reg_it + 16)],
-                                                               exp2_rcp[*(reg_it + 17)],
-                                                               exp2_rcp[*(reg_it + 18)],
-                                                               exp2_rcp[*(reg_it + 19)],
-                                                               exp2_rcp[*(reg_it + 20)],
-                                                               exp2_rcp[*(reg_it + 21)],
-                                                               exp2_rcp[*(reg_it + 22)],
-                                                               exp2_rcp[*(reg_it + 23)]));
-
-            packed_sum = simde_mm256_add_ps(packed_sum,
-                                            simde_mm256_set_ps(exp2_rcp[*(reg_it + 24)],
-                                                               exp2_rcp[*(reg_it + 25)],
-                                                               exp2_rcp[*(reg_it + 26)],
-                                                               exp2_rcp[*(reg_it + 27)],
-                                                               exp2_rcp[*(reg_it + 28)],
-                                                               exp2_rcp[*(reg_it + 29)],
-                                                               exp2_rcp[*(reg_it + 30)],
-                                                               exp2_rcp[*(reg_it + 31)]));
-        }
-
-        // sum up the 4 values in the packed SSE variable
-        float sum = 0.0;
-        float * sum_it = reinterpret_cast<float *>(&packed_sum);
-        sum += *sum_it;
-        sum += *(sum_it + 1);
-        sum += *(sum_it + 2);
-        sum += *(sum_it + 3);
-        sum += *(sum_it + 4);
-        sum += *(sum_it + 5);
-        sum += *(sum_it + 6);
-        sum += *(sum_it + 7);
-
-        // compute first estimate
-        double estimate = alphaMM_float_ / sum;
-
-        // use linear counting of zeros for small values
-        if (estimate <= 2.5 * m_)
-        {
-            uint32_t zeros = 0u;
-
-            for (size_t i = 0; i < m_; ++i)
-            {
-                if (!M_[i])
-                    ++zeros;
-            }
-
-            if (zeros != 0u)
-            {
-                estimate = m_ * std::log(static_cast<double>(m_) / static_cast<double>(zeros));
-            }
-        }
-
-        return estimate;
-    }
+    double merge_and_estimate_SIMD(hyperloglog const & other);
 
     /**
      * Clears all internal registers.
