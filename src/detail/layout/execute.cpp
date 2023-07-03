84cc46c2ee46cd7c69cedf0a99c7fd3db00d1707
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2023-07-03 13:01:53 +0200

    [MISC] Move altering the configuration to ctor of the HIBF.

--- src/detail/layout/execute.cpp
+++ src/detail/layout/execute.cpp
@@ -17,34 +17,12 @@ namespace hibf
 
 size_t execute(hibf::config const & config, hibf::data_store & data)
 {
-    hibf::config config_copy{config};
-
-    if (config_copy.disable_estimate_union)
-        config_copy.disable_rearrangement = true;
-
-    if (config_copy.tmax == 0) // no tmax was set by the user on the command line
-    {
-        // Set default as sqrt(#samples). Experiments showed that this is a reasonable default.
-        if (size_t number_samples = data.kmer_counts->size();
-            number_samples >= 1ULL << 32) // sqrt is bigger than uint16_t
-            throw std::invalid_argument{"Too many samples. Please set a tmax (see help via `-hh`)."}; // GCOVR_EXCL_LINE
-        else
-            config_copy.tmax = hibf::next_multiple_of_64(static_cast<uint16_t>(std::ceil(std::sqrt(number_samples))));
-    }
-    else if (config_copy.tmax % 64 != 0)
-    {
-        config_copy.tmax = hibf::next_multiple_of_64(config_copy.tmax);
-        std::cerr << "[HIBF LAYOUT WARNING]: Your requested number of technical bins was not a multiple of 64. "
-                  << "Due to the architecture of the HIBF, it will use up space equal to the next multiple of 64 "
-                  << "anyway, so we increased your number of technical bins to " << config_copy.tmax << ".\n";
-    }
-
     data.fpr_correction =
-        layout::compute_fpr_correction({.fpr = config_copy.maximum_false_positive_rate, // prevent clang-format
-                                        .hash_count = config_copy.number_of_hash_functions,
-                                        .t_max = config_copy.tmax});
+        layout::compute_fpr_correction({.fpr = config.maximum_false_positive_rate, // prevent clang-format
+                                        .hash_count = config.number_of_hash_functions,
+                                        .t_max = config.tmax});
 
-    return hibf::layout::hierarchical_binning{data, config_copy}.execute();
+    return hibf::layout::hierarchical_binning{data, config}.execute();
 }
 
 } // namespace hibf
