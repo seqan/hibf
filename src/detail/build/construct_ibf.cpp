0cb2977ef435d60baac166d044e2c2f9dc21f440
Enrico Seiler <enrico.seiler@hotmail.de>
2023-08-21 16:36:58 +0200

    [MISC] Use a strong type for bin_size_in_bits

--- src/detail/build/construct_ibf.cpp
+++ src/detail/build/construct_ibf.cpp
@@ -37,9 +37,9 @@ hibf::interleaved_bloom_filter construct_ibf(robin_hood::unordered_flat_set<uint
     auto & node_data = data.node_map[node];
 
     size_t const kmers_per_bin{static_cast<size_t>(std::ceil(static_cast<double>(kmers.size()) / number_of_bins))};
-    double const bin_bits{static_cast<double>(bin_size_in_bits(kmers_per_bin,
-                                                               data.config.number_of_hash_functions,
-                                                               data.config.maximum_false_positive_rate))};
+    double const bin_bits{static_cast<double>(bin_size_in_bits({.fpr = data.config.maximum_false_positive_rate,
+                                                                .hash_count = data.config.number_of_hash_functions,
+                                                                .elements = kmers_per_bin}))};
     hibf::bin_size const bin_size{static_cast<size_t>(std::ceil(bin_bits * data.fpr_correction[number_of_bins]))};
     hibf::bin_count const bin_count{node_data.number_of_technical_bins};
 
