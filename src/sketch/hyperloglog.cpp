6055d7d8dcb3e14548b538093bee4e3a3b09a733
Enrico Seiler <enrico.seiler@hotmail.de>
2023-10-10 19:56:33 +0200

    [FIX] More elegant solution, probably

--- src/sketch/hyperloglog.cpp
+++ src/sketch/hyperloglog.cpp
@@ -59,10 +59,8 @@ void hyperloglog::add(std::string_view const sv)
     M_[index] = std::max(rank, M_[index]);
 }
 
-void hyperloglog::add(uint64_t const value)
+void hyperloglog::add(uint64_t const & value)
 {
-    // chopper hibf_statistics, gcc13 + lto: overzealous optimization results in wrong results
-    asm volatile("" : : "r,m"(value) : "memory");
     uint64_t const hash = XXH3_64bits(&value, sizeof(uint64_t));
     // the first b_ bits are used to distribute the leading zero counts along M_
     uint64_t const index = hash >> (64 - b_);
