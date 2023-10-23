2c40d4f74faff94b68a232cfab8a9f2b0045802c
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2023-10-23 11:59:59 +0200

    [TEST] Add bin_size_in_bits_test.

---
+++ test/unit/hibf/build/bin_size_in_bits_test.cpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <gtest/gtest.h> // for Message, TestPartResult, EXPECT_EQ, Test, ASSERT_EQ, TestInfo, TEST
+
+#include <cstddef>  // for size_t
+#include <optional> // for optional
+#include <vector>   // for vector, allocator
+
+#include <hibf/build/bin_size_in_bits.hpp> // for graph
+
+TEST(bin_size_in_bits_test, general)
+{
+    EXPECT_EQ((seqan::hibf::build::bin_size_in_bits({.fpr = 0.05, .hash_count = 2, .elements = 1000})), 7903u);
+}
+
+TEST(bin_size_in_bits_test, no_elements)
+{
+    EXPECT_EQ((seqan::hibf::build::bin_size_in_bits({.fpr = 0.05, .hash_count = 1, .elements = 0})), 0u);
+}
+
+#ifndef NDEBUG
+TEST(bin_size_in_bits_test, assert_trigger)
+{
+    EXPECT_DEATH((seqan::hibf::build::bin_size_in_bits({.fpr = 0.05, .hash_count = 0, .elements = 10})), "");
+    EXPECT_DEATH((seqan::hibf::build::bin_size_in_bits({.fpr = 0.0, .hash_count = 2, .elements = 10})), "");
+    EXPECT_DEATH((seqan::hibf::build::bin_size_in_bits({.fpr = 1.0, .hash_count = 2, .elements = 10})), "");
+}
+#endif
